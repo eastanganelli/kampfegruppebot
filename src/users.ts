eb5a019f6b75d4d8b2ff7412c124b6e3782da213
ZEQUIST <ezequielstanganelli@gmail.com>
2019-11-13 21:02:48 -0300

    Downgrade Rank

--- src/users.ts
+++ src/users.ts
@@ -55,7 +55,7 @@ export async function downgradingRank(uid: string, client: any) {
                 firebase.database().ref('/users').child(uid).child('connect').update({ lastadv: new Date() });
                 u.addRole(roles[i + 1]);
                 u.removeRole(roles[i]);
-                server.channels.find((c: any) => c.id === 611501862721552386).send('**El USUARIO** <@' + uid +'> fue degrado de <@' + roles[i] + '> a <@' + roles[i+1] + '>');
+                server.guild.channels.get(611501862721552386).send('**El USUARIO** <@' + uid +'> fue degrado de <@' + roles[i] + '> a <@' + roles[i+1] + '>');
             }
         }
     });
@@ -83,13 +83,14 @@ export function checkIfAFK(client: any) {
             if(daydif >= 14 && daydif < 21) {
                 const msg_: string = '<@' + snap.key + '>\nLleva ' + daydif + ' días de **INACTIVIDAD** en el servidor.\nPara dejar de recibir este mensaje, presente actividad. Caso contrario, __cada semana que pase descendera un rango__. Si llega a rango **CANDIDATO**, y no presento actividad, sera expulsado.\nSi tiene rol **INVITADO** o **CANDIDATO**, al vencer la semana de advertencia, __será expulsado directamente__.\nKMPF';
                 client.users.get(String(snap.key)).send(msg_).then(() => { usersfb.child(String(snap.key)).child('connect').update({ lastadv: getWeekNumber() }); });
-                client.guilds.find((g: any) => g.id == serverID).channels.find((c: any) => c.id === 611501862721552386).send('**El USUARIO** <@' + snap.key +'> Se encuentra inactivo hace ' + daydif + ' diás en el servidor\n Mensaje de Advertencia fue _ENVIADO_');
+                client.guilds.find((g: any) => g.id == serverID).channels.get(611501862721552386).send('**El USUARIO** <@' + snap.key +'> Se encuentra inactivo hace ' + daydif + ' diás en el servidor\n Mensaje de Advertencia fue _ENVIADO_');
             } else if (daydif >= 21) {
                 if(Number(auxuser.connect.lastAdv) < getWeekNumber()) {
                     const msg_: string = '<@' + snap.key + '>\nSu rango fue __DESCENDIDO__!\nSeguira descendiendo, hasta que presente actividad.\n:warning::warning:Recuerde: **Si llega a rango __CANDIDATO__, y no presento actividad, sera expulsado.**:warning::warning:\nKMPF';
                     client.users.get(String(snap.key)).send(msg_).then(() => { usersfb.child(String(snap.key)).child('connect').update({ lastadv: getWeekNumber() }); });
+                    downgradingRank(String(snap.key), client)
                 }
-            }
+            } else { usersfb.child(String(snap.key)).child('connect/lastadv').remove(); }
         });
     });
 }
