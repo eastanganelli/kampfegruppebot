7df2ee58d15fad08335a27480d4f554d0db0dbc5
Ezequiel Augusto Stanganelli <ezequielstanganelli@gmail.com>
2022-04-02 15:24:47 -0300

    Delete dist directory

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    "token": "NTk0NTcxMzExMTcxMzcxMDA4.XS0CTg.cBcnsfooM9yzWrU8w4bqGqkUObY",
-    "prefix": "?"
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBVyxRQUFBLE1BQU0sR0FBRztJQUNoQixPQUFPLEVBQUUsNkRBQTZEO0lBQ3RFLFFBQVEsRUFBRSxHQUFHO0NBQ2hCLENBQUEifQ==
--- dist/ie-code.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaWUtY29kZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9pZS1jb2RlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
